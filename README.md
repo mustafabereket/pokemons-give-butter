c0a926b4e138b8eb380878c3bcf9f468883b0334
mustafabereket <mustafa.bereket@outlook.com>
2022-09-22 05:19:57 +0300

    - App Submission

--- README.md
+++ README.md
@@ -47,7 +47,21 @@ When you are finished, please upload your completed work to your Github and invi
 Please take some time to answer the following questions. Your answers should go directly in this `readme`.
 
 - Given more time, what would you suggest for improving the performance of this app?
-
+    - There are definitely things I can think of make this app better if I had more time and permission and authority, off top of my head:
+      - Error Handling: calls in api.js file doesnt handle errors, I didnt add anything in App.js either given time restriction, but I would definitely integrate better error handling. Using react queries (tanstack-useQuery) would work nicely.
+      - Better component structure & State management: I know it s a small app, but I think we could still make use of ContextAPI and proper folder structure of components
+      - Better Styling Tailwind|SASS|SCSS: I think it would have been nicer if we made use of modern styling tools, it would be less to zero CSS code writing especially with tailwind, I love tailwind:)
+      - We are fetching a lot more data than what we display: in an ideal world, we should only fetch what we need, if we had authonmy to build our own stuff, graphql or some sort of midlayer would make payloads much lighter.
+      - Caching: I would cache the initial list, but I wasnt sure if that was part of the ask.
+      - Better TypeChecking: I didnt add any typechecking for time sake, but it d be nice to convert this project into TypeScript or add some simple type enforcement.
+      - I would try to add more colorful and engaging things, change the layout, add pagination
+      - I would make the search functionality more broad (with real API calls) rather than searching within only 151 results. Might add debounce for keystrokes
 - Is there anything you would consider doing if we were to go live with this app?
+  - All of the above plus:
+  - I would use TypeScript
+  - I would add Tests/CI/CD
+  - I would think about security stuff, maybe add prevention methods for abuse (eg DDoS) and maybe load balancers in the deploy side
 
 - What was the most challenging aspect of this work for you (if at all)?
+  - My self-doubt about Pokemon knowledge & Evolution Chain Debugging:
+    - It wasnt very difficult to write the code to traverse the chain but I noticed something in the api results that got me thinking, which was, bulbasaur eventually evolves to venusaur but when I click on venusaur to see its evolution chain I see whole bunch of different pokemons(squirtle,wartortle, blastoise). I was expecting to see no further evolution of venusaur. But I double checked the api results and just accepted the data as they are. But best to my knowledge venusaur is the final form of bulbasaur. That being said, maybe I indeed wrongly implemented this thing, I hope not! fingers crossed :)
--- src/App.js
+++ src/App.js
@@ -1,36 +1,92 @@
 import { useEffect, useState } from "react";
-import { fetchAllPokemon } from "./api";
+import {fetchAllPokemon, fetchEvolutionChainById, fetchPokemonDetailsByName} from "./api";
 
 function App() {
     const [pokemonIndex, setPokemonIndex] = useState([])
     const [pokemon, setPokemon] = useState([])
     const [searchValue, setSearchValue] = useState('')
-    const [pokemonDetails, setPokemonDetails] = useState()
+    const [pokemonDetails, setPokemonDetails] = useState(null);
+    const [loading, setLoading] = useState(false);  // Loading state
 
     useEffect(() => {
         const fetchPokemon = async () => {
             const {results: pokemonList} = await fetchAllPokemon()
-
             setPokemon(pokemonList)
             setPokemonIndex(pokemonList)
         }
-
         fetchPokemon().then(() => {
             /** noop **/
         })
-    }, [searchValue])
+    }, [])
 
     const onSearchValueChange = (event) => {
         const value = event.target.value
         setSearchValue(value)
-
         setPokemon(
-            pokemonIndex.filter(monster => !monster.name.includes(value))
+            pokemonIndex.filter(monster => monster.name.includes(value)) // first bug here, removed '!'
         )
+    }
+
+    const renderList = (list) => {
+        return (
+            <div className={'pokedex__search-results'}>
+                {
+                    list.map(monster => {
+                        return (
+                            <div className={'pokedex__list-item'} key={monster.name}>
+                                <div>
+                                    {monster.name}
+                                </div>
+                                <button onClick={onGetDetails(monster.name)}>Get Details</button>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+
+    const displayContent = () => {
+        // This is main logic for displaying the right list (pokemon or pokemonIndex)
+        if(pokemon.length > 0 && searchValue) {
+            // return following if there are search results
+            return renderList(pokemon);
+        }
+
+        if(pokemon.length === 0 && searchValue) {
+            // return following if there is search value but no result
+            return (<p>No Results Found</p>)
+        }
+
+        // else return our main list (pokemonIndex)
+        return renderList(pokemonIndex);
+    }
+
+    const getEvolutionChain = (chain) => {
+        // Evolution chain
+        // BFS until we hit empty evolves to
+        const result = chain.species?.name ? [chain.species?.name] : [];
+        let evolvesTo = chain.evolves_to || [];
+
+        while (evolvesTo.length) {
+            let evolved = evolvesTo.shift();
+            result.push(evolved.species.name);
+            evolvesTo = evolved.evolves_to;
+        }
+
+        return result;
     }
 
     const onGetDetails = (name) => async () => {
         /** code here **/
+        if(pokemonDetails?.name !== name) {
+            setLoading(true);
+            const result = await fetchPokemonDetailsByName(name);
+            const evo = await fetchEvolutionChainById(result.id);
+            result.evolutionChain = getEvolutionChain(evo.chain);
+            setPokemonDetails(result);
+            setLoading(false);
+        }
     }
 
     return (
@@ -39,32 +95,48 @@ function App() {
                 <input value={searchValue} onChange={onSearchValueChange} placeholder={'Search Pokemon'}/>
             </div>
             <div className={'pokedex__content'}>
-                {pokemon.length > 0 && (
-                    <div className={'pokedex__search-results'}>
-                        {
-                            pokemon.map(monster => {
-                                return (
-                                    <div className={'pokedex__list-item'} key={monster.name}>
-                                        <div>
-                                            {monster.name}
-                                        </div>
-                                        <button onClick={onGetDetails(monster.name)}>Get Details</button>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                )}
+                {displayContent()}
                 {
                     pokemonDetails && (
                         <div className={'pokedex__details'}>
                             {/*  code here  */}
+                            {loading ? <p>Loading...</p> : <Card pokemonDetails={pokemonDetails} />}
                         </div>
                     )
                 }
             </div>
         </div>
     );
+}
+
+const Card = ({pokemonDetails}) => {
+    const moves = pokemonDetails?.moves?.slice(0,4) || [<li>No Moves Found</li>]; // slicing first 4 to match mock-up
+    const types = pokemonDetails?.types?.slice(0,4) || [<li>No Types Found</li>];
+    return (
+        <>
+                <span className={"pokedex__title"}>
+                    {pokemonDetails.name}
+                </span>
+            <div className={"pokedex__skills"}>
+                <div>
+                    <div className={"pokedex__title"}>Types</div>
+                    <ul>
+                        {types.map(type => <li key={type.type.name}>{type.type.name}</li>)}
+                    </ul>
+                </div>
+                <div>
+                    <div className={"pokedex__title"}>Moves</div>
+                    <ul>
+                        {moves.map(move => <li key={move.move.name}>{move.move.name}</li>)}
+                    </ul>
+                </div>
+            </div>
+            <div className={'pokedex__evs'}>
+                <div className="pokedex__title">Evolutions</div>
+                <div>{pokemonDetails.evolutionChain.map(evs => <span key={evs} className={'pokedex__evs_items'}>{evs}</span>)}</div>
+            </div>
+        </>
+    )
 }
 
 export default App;
